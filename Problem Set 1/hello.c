37d2a7aace17c1f244648336421899d73384a28d
Abishek Ghose <102427714+imabishekghose29@users.noreply.github.com>
2022-07-26 16:03:16 +0530

    Update hello.c

--- Problem Set 1/hello.c
+++ Problem Set 1/hello.c
@@ -2,6 +2,9 @@
 #include <cs50.h>
 int main(void)
 {
+    //Gets the name from the user
     string name = get_string("Whats's your name?\n");
+    
+    //Prints out the salutation plus the name
     printf("hello, %s\n", name);
 }
